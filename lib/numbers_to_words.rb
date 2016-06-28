381689a48b661c81dc8f832601e0d0a26b07f6fa
Brian Grant and Sky Rousse <brian@>
2016-06-28 10:32:05 -0700

    add new spec

--- lib/numbers_to_words.rb
+++ lib/numbers_to_words.rb
@@ -1,15 +1,19 @@
-class Fixnum
+class String
   define_method(:numbers_to_words) do
-    one_to_nine = { 1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine" }
-    ten_to_nineteen = { 10 => "ten", 11 => "eleven", 12 => "twelve", 13 => "thirteen", 14 => "fourteen", 15 => "fifthteen", 16 => "sixteen", 17 => "seventeen", 18 => "eighteen", 19 => "nineteen"}
-    twenty_to_ninety = { 20 => "twenty", 30 => "thirty", 40 => "fourty", 50 => "fifty", 60 => "sixty", 70 => "seventy", 80 => "eighty", 90 => "ninety"}
-    digits = self.to_s.size
+    one_to_nine = { "1" => "one", "2" => "two", "3" => "three", "4" => "four", "5" => "five", "6" => "six", "7" => "seven", "8" => "eight", "9" => "nine" }
+    ten_to_nineteen = { "10" => "ten", "11" => "eleven", "12" => "twelve", "13" => "thirteen", "14" => "fourteen", "15" => "fifthteen", "16" => "sixteen", "17" => "seventeen", "18" => "eighteen", "19" => "nineteen" }
+    twenty_to_ninety = { "2" => "twenty", "3" => "thirty", "4" => "fourty", "5" => "fifty", "6" => "sixty", "7" => "seventy", "8" => "eighty", "9" => "ninety"}
+    digits = self.size
+    numbers = self.split("")
+    result = []
     if digits.eql?(1)
-      one_to_nine.fetch(self)
-    elsif (10..19).include?(self)
-      ten_to_nineteen.fetch(self)
-    elsif digits.eql?(2).&self.>(19)
-      twenty_to_ninety.fetch(self)
+      result.push(one_to_nine.fetch(self))
+    elsif ten_to_nineteen.include?(self)
+      result.push(ten_to_nineteen.fetch(self))
+    elsif digits.eql?(2).&self.to_i.>(19)
+      result.push(twenty_to_ninety.fetch(numbers.at(0)))
+      result.push(one_to_nine.fetch(numbers.at(1)))
     end
+    result.join(" ")
   end
 end
--- spec/numbers_to_words_spec.rb
+++ spec/numbers_to_words_spec.rb
@@ -1,14 +1,14 @@
 require ('rspec')
 require ('numbers_to_words.rb')
 
-describe('Fixnum#numbers_to_words') do
+describe('String#numbers_to_words') do
   it('returns a word for a single digit number') do
-    expect(1.numbers_to_words()).to(eq("one"))
+    expect("1".numbers_to_words()).to(eq("one"))
   end
   it('returns a word for a number between 10 and twenty') do
-    expect(13.numbers_to_words()).to(eq("thirteen"))
+    expect("13".numbers_to_words()).to(eq("thirteen"))
   end
   it('returns a word for a number between 21 and 99') do
-    expect(30.numbers_to_words()).to(eq("thirty"))
+    expect("34".numbers_to_words()).to(eq("thirty four"))
   end
 end
